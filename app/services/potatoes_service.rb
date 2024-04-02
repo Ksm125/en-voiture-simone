adfb649c47b750f4d5d8f9d34f0aa51b6dbd247b
Kassam <kassam.housseny@gmail.com>
2024-04-02 11:26:54 +0200

    Add best gains method to potatoes_service.rb

--- app/services/potatoes_service.rb
+++ app/services/potatoes_service.rb
@@ -2,6 +2,9 @@
 
 # Service to handle potatoes data
 class PotatoesService
+  MAX_POTATOES = 100
+  GAINS_CURRENCY = '€'
+
   def initialize(potato)
     @potato = potato
   end
@@ -9,5 +12,50 @@ class PotatoesService
   # Get the daily prices of the potato for a given date
   def daily_prices(date)
     @potato.prices.select { |price| price.time.to_date == date.to_date }.sort_by(&:time)
+  end
+
+  # Return the best gains of the potato for a given date
+  # The best gains are the prices that have the highest difference between them
+  # Knowing that i can only buy 100 potatoes and sell them at the end of the day
+  def best_gains(date)
+    prices = daily_prices(date)
+    # We can't have any gain if we have less than 2 prices
+    if prices.size < 2
+      return {
+        buy: nil,
+        sell: nil,
+        best_gain: "0#{GAINS_CURRENCY}"
+      }
+    end
+
+    best_buy = prices[0]
+    best_sell = prices[1]
+    best_gain = (best_sell.value - best_buy.value) * MAX_POTATOES
+
+    i = 0 # Start from the second price since we already have the first price
+    while i < prices.size
+      j = i + 1
+      while j < prices.size
+        buy = prices[i]
+        sell = prices[j]
+        gain = (sell.value - buy.value) * MAX_POTATOES
+        if gain > best_gain
+          best_gain = gain
+          best_buy = buy
+          best_sell = sell
+          # we can skip the next prices since we already have the best buy and sell.
+          # There is no need to check the prices between the best buy and sell since we can't have a better gain
+          i = j + 1
+        end
+        j += 1
+      end
+      i += 1
+    end
+
+    {
+      buy: best_buy,
+      sell: best_sell,
+      best_gain: "#{best_gain}#{GAINS_CURRENCY}"
+    }
   end
 end
--- spec/services/potatoes_service_spec.rb
+++ spec/services/potatoes_service_spec.rb
@@ -23,4 +23,28 @@ describe PotatoesService do
                              Price.new(Time.parse('2021-01-02 02:00:00'), 800)])
     end
   end
+
+  describe '#best_gains' do
+    it 'returns the best gains of the potato for a given date' do
+      service = PotatoesService.new(potato)
+
+      date = Date.parse('2021-01-02')
+      gains = service.best_gains(date)
+
+      expect(gains).to eq(
+        buy: Price.new(Time.parse('2021-01-02 00:00:00'), 200),
+        sell: Price.new(Time.parse('2021-01-02 02:00:00'), 800),
+        best_gain: "#{PotatoesService::MAX_POTATOES * (800 - 200)}#{PotatoesService::GAINS_CURRENCY}"
+      )
+    end
+
+    it 'returns 0 if there are less than 2 prices' do
+      service = PotatoesService.new(potato)
+
+      date = Date.parse('2021-01-03')
+      gains = service.best_gains(date)
+
+      expect(gains).to eq({ buy: nil, sell: nil, best_gain: '0€'})
+    end
+  end
 end
